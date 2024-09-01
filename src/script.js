13d85e7d2650157e4e409c4f6b8ab3aa8db982d5
dielies <160782509+dielies@users.noreply.github.com>
2024-09-01 17:22:02 +0200

    injected current weather data

--- src/script.js
+++ src/script.js
@@ -1,8 +1,39 @@
 function refreshWeather(response) {
   let temperatureValue = document.querySelector("#current-temp-value");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let date = new Date(response.data.time * 1000);
+  let timeElement = document.querySelector("#time");
+
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
   temperatureValue.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
